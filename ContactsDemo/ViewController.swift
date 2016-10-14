e33c46325a15b549e0e220260f1fc2cb838ee1bc
Alex Ackerman <development@darkhonor.com>
2016-10-13 21:35:28 -0400

    Initial Commit

---
+++ ContactsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactsDemo
+//
+//  Created by Alex Ackerman on 10/13/16.
+//  Copyright © 2016 Darkhonor Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
